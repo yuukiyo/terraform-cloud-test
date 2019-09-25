ac4cf2c8982da93d144391c682043e72741021ac
Yuki Yoshida <yshdyk11202000@gmail.com>
2019-09-25 14:08:25 +0900

    hoge

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@ variable "aws_access_key" {}
 variable "aws_secret_key" {}
 
 provider "aws" {
-  access_key = "var.aws_access_key"
-  secret_key = "var.aws_secret_key"
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
   region     = "ap-northeast-1"
 }
 
